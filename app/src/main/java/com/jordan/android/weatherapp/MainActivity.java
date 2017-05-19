ec48c868987f5ebc029f2b46c6967793889b5cd5
Jordan Brooks <jordbrooks14@gmail.com>
2017-05-19 00:11:01 -0700

    Set up AsyncTask to fetch weather data and display JSON response in a TextView

--- app/src/main/java/com/jordan/android/weatherapp/MainActivity.java
+++ app/src/main/java/com/jordan/android/weatherapp/MainActivity.java
@@ -1,16 +1,62 @@
 package com.jordan.android.weatherapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.jordan.android.weatherapp.Utilities.NetworkUtilities;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView forecastTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NetworkUtilities.buildURL("Vancouver");
+
+        forecastTextView = (TextView) findViewById(R.id.weather_information);
+
+        fetchWeatherData();
     }
+
+    private void fetchWeatherData() {
+        new FetchWeatherTask().execute("Vancouver");
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            if (params.length == 0) {
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestURL = NetworkUtilities.buildURL(location);
+
+            try {
+                String response = NetworkUtilities.getHttpResponse(weatherRequestURL);
+                return response;
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("Null!");
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String weatherData) {
+            if (weatherData != null) {
+                forecastTextView.setText(weatherData);
+            }
+        }
+    }
+
 }

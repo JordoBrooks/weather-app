610e5e83beecc1c6f7c13b33411fb05aca30aaf1
Jordan Brooks <jordbrooks14@gmail.com>
2017-05-21 19:18:03 -0700

    Add share functionality to day detail activity (used ShareCompat for intent building)

--- app/src/main/java/com/jordan/android/weatherapp/DayDetailActivity.java
+++ app/src/main/java/com/jordan/android/weatherapp/DayDetailActivity.java
@@ -3,11 +3,16 @@ package com.jordan.android.weatherapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * Created by jordan on 21/05/17.
+ *  Activity for particular day showing more weather detail
  */
 
 public class DayDetailActivity extends AppCompatActivity {
@@ -29,5 +34,28 @@ public class DayDetailActivity extends AppCompatActivity {
             weatherForThisDay = parentIntent.getStringExtra(Intent.EXTRA_TEXT);
             dayDetailTextView.setText(weatherForThisDay);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.day_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            Intent intentToShare = ShareCompat.IntentBuilder.from(this)
+                    .setType("text/plain")
+                    .setText(weatherForThisDay)
+                    .getIntent();
+
+            startActivity(intentToShare);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/jordan/android/weatherapp/MainActivity.java
+++ app/src/main/java/com/jordan/android/weatherapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.jordan.android.weatherapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -87,5 +91,33 @@ public class MainActivity extends AppCompatActivity implements WeatherClickHandl
         Intent intentToOpenDayDetailActivity = new Intent(this, DayDetailActivity.class);
         intentToOpenDayDetailActivity.putExtra(Intent.EXTRA_TEXT, weatherForTheDayClicked);
         startActivity(intentToOpenDayDetailActivity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_map) {
+            String locationString = "Vancouver, BC";
+            Uri encodedLocation = Uri.parse("geo:0,0?q=" + locationString);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(encodedLocation);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "No appropriate map app installed!", Toast.LENGTH_SHORT);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

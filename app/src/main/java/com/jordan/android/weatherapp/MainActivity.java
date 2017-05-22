a7ec9b48d818cc68d4367b5290288c74838a6645
Jordan Brooks <jordbrooks14@gmail.com>
2017-05-22 00:50:33 -0700

    Replace AsyncTask with AsyncTaskLoader

--- app/src/main/java/com/jordan/android/weatherapp/MainActivity.java
+++ app/src/main/java/com/jordan/android/weatherapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.jordan.android.weatherapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +13,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,11 +26,15 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements WeatherClickHandler {
+public class MainActivity extends AppCompatActivity implements WeatherClickHandler, LoaderManager.LoaderCallbacks<String[]> {
 
     private RecyclerView recyclerView;
 
     private WeatherAdapter weatherAdapter;
+
+    private ProgressBar loadingCircle;
+
+    private static final int WEATHER_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements WeatherClickHandl
 
         recyclerView = (RecyclerView) findViewById(R.id.weather_rv);
 
+        loadingCircle = (ProgressBar) findViewById(R.id.loading_circle);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -41,49 +52,9 @@ public class MainActivity extends AppCompatActivity implements WeatherClickHandl
 
         recyclerView.setAdapter(weatherAdapter);
 
-        fetchWeatherData();
-    }
+        LoaderManager.LoaderCallbacks<String[]> callback = this;
 
-    /**
-     *  Uses background AsyncTask to load in weather data
-     */
-    private void fetchWeatherData() {
-        new FetchWeatherTask().execute("Vancouver");
-    }
-
-
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-
-        @Override
-        protected String[] doInBackground(String... params) {
-
-            if (params.length == 0) {
-                return null;
-            }
-
-            String location = params[0];
-            URL weatherRequestURL = NetworkUtilities.buildURL(location);
-
-            try {
-                String response = NetworkUtilities.getHttpResponse(weatherRequestURL);
-                String[] parsedWeather = OpenWeatherMapParser.parseWeather(MainActivity.this, response);
-                return parsedWeather;
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("IOException thrown!");
-                return null;
-            } catch (JSONException e) {
-                e.printStackTrace();
-                System.out.println("JSONException thrown!");
-                return null;
-            }
-
-        }
-
-        @Override
-        protected void onPostExecute(String[] weatherData) {
-            weatherAdapter.setWeatherData(weatherData);
-        }
+        getSupportLoaderManager().initLoader(WEATHER_LOADER_ID, null, callback);
     }
 
     @Override
@@ -104,6 +75,12 @@ public class MainActivity extends AppCompatActivity implements WeatherClickHandl
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
+        if (id == R.id.action_refresh) {  // REMOVE EVENTUALLY
+            invalidateData();
+            getSupportLoaderManager().restartLoader(0, null, this);
+            return true;
+        }
+
         if (id == R.id.action_map) {
             String locationString = "Vancouver, BC";
             Uri encodedLocation = Uri.parse("geo:0,0?q=" + locationString);
@@ -119,5 +96,60 @@ public class MainActivity extends AppCompatActivity implements WeatherClickHandl
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void invalidateData() {       // REMOVE EVENTUALLY
+        weatherAdapter.setWeatherData(null);
+    }
+
+    @Override
+    public Loader<String[]> onCreateLoader(int id, Bundle args) {
+
+        return new AsyncTaskLoader<String[]>(this) {
+
+            String[] weatherData = null;
+
+            @Override
+            protected void onStartLoading() {
+                if (weatherData != null) {
+                    deliverResult(weatherData);
+                } else {
+                    loadingCircle.setVisibility(View.VISIBLE);
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public String[] loadInBackground() {
+
+                URL weatherRequestURL = NetworkUtilities.buildURL("Vancouver");
+
+                try {
+                    String response = NetworkUtilities.getHttpResponse(weatherRequestURL);
+                    String[] parsedWeather = OpenWeatherMapParser.parseWeather(MainActivity.this, response);
+                    return parsedWeather;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("IOException thrown!");
+                    return null;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    System.out.println("JSONException thrown!");
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String[]> loader, String[] data) {
+        loadingCircle.setVisibility(View.INVISIBLE);
+        weatherAdapter.setWeatherData(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String[]> loader) {
+
     }
 }
